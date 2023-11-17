a67e225bb1a7e475ed24cc62a12d9a4396acd3cb
Elbarae <elbarae1921@gmail.com>
2023-11-17 02:14:41 +0100

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -6,14 +6,11 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
     input: 'src/main.ts',
     output: {
-        file: '/home/elbarae/.config/Screeps/scripts/46_101_251_59___21025/default/main.js',
+        // file: '/home/elbarae/.config/Screeps/scripts/46_101_251_59___21025/default/main.js',
+        file: 'dist/main.js',
         format: 'cjs',
         sourcemap: true
     },
 
-    plugins: [
-        resolve({ rootDir: 'src' }),
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.json' })
-    ]
+    plugins: [resolve({ rootDir: 'src' }), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
 };
