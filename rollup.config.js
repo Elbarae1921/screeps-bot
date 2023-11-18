7ac6e6963a503610bdbdbe872541ef29908517d8
Elbarae <elbarae1921@gmail.com>
2023-11-18 02:19:15 +0100

    feat: add rollup plugin to upload code to the screeps API

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,29 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import http from 'http';
+import 'dotenv/config';
+
+const uploadCode = code => {
+    const data = {
+        branch: 'default',
+        modules: {
+            main: code
+        }
+    };
+    const req = http.request({
+        hostname: process.env.API_HOSTNAME,
+        port: process.env.API_PORT,
+        path: '/api/user/code',
+        method: 'POST',
+        auth: process.env.API_USER_EMAIL + ':' + process.env.API_USER_PASSWORD,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    });
+    req.write(JSON.stringify(data));
+    req.end();
+};
 
 export default {
     input: 'src/main.ts',
@@ -12,5 +35,16 @@ export default {
         sourcemap: true
     },
 
-    plugins: [resolve({ rootDir: 'src' }), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    plugins: [
+        resolve({ rootDir: 'src' }),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        {
+            name: 'upload-code',
+            generateBundle(_outputOptions, bundle) {
+                const entry = Object.values(bundle).find(chunk => chunk.isEntry);
+                uploadCode(entry.code);
+            }
+        }
+    ]
 };
